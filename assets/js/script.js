9e51ee1a0810dfc219a1aaae3679850641ba0735
hrcoop16 <hrcoop16@gmail.com>
2022-08-09 17:06:54 -0500

    HTML layout for data completed

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// API information
+var APIurl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={APIkey}';
+var APIKey = 'deea8678f358f6e59a970dd133b9edc3';
